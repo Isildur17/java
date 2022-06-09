ea49371201152fe4db277f78acac98d110f4023f
Isildur17 <toisildur1@gmail.com>
2022-06-10 01:14:52 +0300

    Adding abstract class; adding separate methods for calculator actions; handling dividing to zero

--- Calculator/src/main/java/com/emerline/sandbox/Calculator.java
+++ Calculator/src/main/java/com/emerline/sandbox/Calculator.java
@@ -1,17 +1,19 @@
 package com.emerline.sandbox;
 
-import java.util.Scanner;
-
-public abstract class Calculator {
-
-    protected double calculation_result;
-    static String GET_DOUBLE_VALUE_EN = "Enter a decimal with a dot separator";
-    static String CHOOSE_OPERATOR_EN = "Select operator: \n + \n - \n * \n /";
-    static String PATTERN = "[|/+*-]";
-    static String CALCULATION_RESULTS_EN ="The results of calculation is:";
-
-    public abstract double getEnteredValue(Scanner scanner);
-    public abstract String getEnteredOperator (Scanner scanner);
-    public abstract void performCalculation(double value1, double value2, String operation);
+public interface Calculator {
+    String GET_DOUBLE_SECOND_VALUE_EN = "Enter the first value. The decimal value should have a dot separator";
+    String GET_DOUBLE_FIRST_VALUE_EN = "Enter the second value. The decimal value should have a dot separator";
+    String CHOOSE_OPERATOR_EN = "Select operator: \n + \n - \n * \n /";
+    String PATTERN = "[|/+*-]";
+    String CALCULATION_RESULTS_EN = "The results of calculation is:";
+    String ZERO_DIVIDER_EXCEPTION_EN = "You can't divide by zero. Please enter a second number other than zero";
+    double getEnteredFirstValue();
+    double getEnteredSecondValue();
+    String getEnteredOperator ();
+    void performCalculation(double value1, double value2, String operation);
+    double multiplicationOperation(double value1, double value2);
+    double additionOperation(double value1, double value2);
+    double divisionOperation(double value1, double value2);
+    double subtractionOperation(double value1, double value2);
 }
 
--- Calculator/src/main/java/com/emerline/sandbox/CalculatorForActions.java
+++ Calculator/src/main/java/com/emerline/sandbox/CalculatorForActions.java
@@ -2,20 +2,33 @@ package com.emerline.sandbox;
 
 import java.util.Scanner;
 
-class CalculatorForActions extends Calculator {
+class CalculatorForActions implements Calculator {
+    private double calculationResult;
 
     @Override
-    public double getEnteredValue(Scanner scanner) {
-        System.out.println(GET_DOUBLE_VALUE_EN);
+    public double getEnteredFirstValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(GET_DOUBLE_FIRST_VALUE_EN);
         while (!scanner.hasNextDouble()) {
-            System.out.println(GET_DOUBLE_VALUE_EN);
+            System.out.println(GET_DOUBLE_FIRST_VALUE_EN);
+            scanner.next();
+        }
+        return scanner.nextDouble();
+    }
+    @Override
+    public double getEnteredSecondValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(GET_DOUBLE_SECOND_VALUE_EN);
+        while (!scanner.hasNextDouble()) {
+            System.out.println(GET_DOUBLE_SECOND_VALUE_EN);
             scanner.next();
         }
         return scanner.nextDouble();
     }
 
     @Override
-    public String getEnteredOperator(Scanner scanner) {
+    public String getEnteredOperator() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(CHOOSE_OPERATOR_EN);
         while (!scanner.hasNext(PATTERN)) {
             System.out.println(CHOOSE_OPERATOR_EN);
@@ -23,24 +36,47 @@ class CalculatorForActions extends Calculator {
         }
         return scanner.next();
     }
+    @Override
+    public double multiplicationOperation(double value1, double value2) {
+        return value1 * value2;
+    }
+
+    @Override
+    public double additionOperation(double value1, double value2) {
+        return value1 + value2;
+    }
+
+    @Override
+    public double divisionOperation(double value1, double value2) {
+        return value1 / value2;
+    }
+
+    @Override
+    public double subtractionOperation(double value1, double value2) {
+        return value1 - value2;
+    }
 
     @Override
     public void performCalculation(double value1, double value2, String operation) {
         switch (operation) {
             case "+":
-                calculation_result = value1 + value2;
+                calculationResult = additionOperation(value1, value2);
                 break;
             case "-":
-                calculation_result = value1 - value2;
+                calculationResult = subtractionOperation(value1, value2);
                 break;
             case "*":
-                calculation_result = value1 * value2;
+                calculationResult = multiplicationOperation(value1, value2);
                 break;
             case "/":
-                calculation_result = value1 / value2;
+                while (value2 == 0) {
+                    System.out.println(ZERO_DIVIDER_EXCEPTION_EN);
+                    value2 = this.getEnteredFirstValue();
+                }
+                calculationResult = divisionOperation(value1, value2);
                 break;
         }
         System.out.println(CALCULATION_RESULTS_EN);
-        System.out.println(value1 + " " + operation + " " + value2 + " = " + calculation_result);
+        System.out.println(value1 + " " + operation + " " + value2 + " = " + calculationResult);
     }
 }
